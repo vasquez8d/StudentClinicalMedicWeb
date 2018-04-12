31674619b953a3bcdaabe776ebdfbafd33bc2a86
vasquez8d <vasquez8d@gmail.com>
2018-04-11 20:57:30 -0500

    mantenimiento usuarios

--- src/app/global/globalvalues.ts
+++ src/app/global/globalvalues.ts
@@ -3,18 +3,18 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class GlobalValues {
     urlAuthUser() {
-        // return 'https://studentav.herokuapp.com/user';
-        return 'http://localhost:1337/user';
+        return 'https://studentav.herokuapp.com/user';
+        // return 'http://localhost:1337/user';
     }
 
     urlAuthUserAcademy() {
-        // return 'https://studentav.herokuapp.com/useracademy';
-        return 'http://localhost:1337/useracademy';
+        return 'https://studentav.herokuapp.com/useracademy';
+        // return 'http://localhost:1337/useracademy';
     }
 
     urlValidateMail(){
-        // return 'https://studentav.herokuapp.com/emailauth';
-        return 'http://localhost:1337/emailauth';
+        return 'https://studentav.herokuapp.com/emailauth';
+        // return 'http://localhost:1337/emailauth';
     }
 
     cryptoKey(){
